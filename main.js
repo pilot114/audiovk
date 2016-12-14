14bb2725901f8b45173c76f87953b1d328472b52
pilot114 <pilot114@bk.ru>
2016-12-14 14:42:23 +0700

    update

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function readFileToArray(filepath) {
   return new Promise(function(resolve, reject) {
     var cb = function(fileContent) {
       var arrayOfLines = fileContent.match(/[^\r\n]+/g);
-            resolve(arrayOfLines);
+      resolve(arrayOfLines);
     };
     readFile(filepath, cb);
     });
@@ -37,7 +37,7 @@ function* nextIdGen(ids) {
   }
 }
 
-function createWindow () {
+function createParseWindow(contentScript, ids) {
 
   // one load
   mainWindow = new BrowserWindow({width: 1080, height: 860, darkTheme: true, resizable: false,
@@ -59,66 +59,27 @@ function createWindow () {
   })
 
   /////////////////////////////////////////////////////////////
-  readFile('./exec.js', function(contentScript){
+  var idsLen = ids.length;
+  var gen = nextIdGen(ids);
 
-    readFileToArray('./ids.txt').then( ids => {
-      var idsLen = ids.length;
-      var gen = nextIdGen(ids);
-
-      var id = gen.next().value;
-      console.log("load page: " + "http://vk.com/audios" + id + '#' + id);
-      mainWindow.loadURL("http://vk.com/audios" + id + '#' + id);
-      mainWindow.webContents.executeJavaScript(contentScript);
-
-      MongoClient.connect(conUrl, function(err, db) {
-        assert.equal(null, err);
-        console.log("Connected successfully to server");
-
-        ipcMain.on('parse_result', (event, data) => {
-
-          // access denied
-          if (data[0] == "error") {
-            var id = gen.next().value;
-            console.log("load page: " + "http://vk.com/audios" + id + '#' + id);
-            mainWindow.loadURL("http://vk.com/audios" + id + '#' + id);
-            mainWindow.webContents.executeJavaScript(contentScript);
-          } else {
-            console.log("Parsed user with ID: " + data[0])
-            console.log("Audios count: " + data.slice(1).length)
-            var doc = {
-              'uid' : data[0],
-              'audios' : data.slice(1)
-            };
-
-            insertDocumentWithUpdate(db, function() {
-              console.log('success insert!');
-              var id = gen.next().value;
-              console.log("load page: " + "http://vk.com/audios" + id + '#' + id);
-              mainWindow.loadURL("http://vk.com/audios" + id + '#' + id);
-              mainWindow.webContents.executeJavaScript(contentScript);
-            }, doc);
-          }
-
-          event.returnValue = 'ok'
-        })
-
-        // db.close();
-      });
-    });
-  });
+  var id = gen.next().value;
+  console.log("load page: " + "http://vk.com/audios" + id + '#' + id);
+  mainWindow.loadURL("http://vk.com/audios" + id + '#' + id);
+  mainWindow.webContents.executeJavaScript(contentScript);
   /////////////////////////////////////////////////////////////
 }
 
 // command interface
 function createCommandWindow(){
-  commandWindow = new BrowserWindow({width: 600, height: 600, darkTheme: true, resizable: false,
+  commandWindow = new BrowserWindow({width: 600, height: 800, darkTheme: true, resizable: false,
     webPreferences: {
+      defaultEncoding: "utf-8",
       nodeIntegration: true,
       webSecurity: true,
       preload: path.resolve(path.join(__dirname, './preload.js'))
     },
   });
-  commandWindow.setMenu(null);
+  // commandWindow.setMenu(null);
   commandWindow.loadURL(`file://${__dirname}/command.html`)
   commandWindow.on('closed', function () {
     commandWindow = null
@@ -137,9 +98,72 @@ var insertDocumentWithUpdate = function(db, cb, doc) {
   });
 }
 
+ipcMain.on('parse_result', (event, data) => {
+  MongoClient.connect(conUrl, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    readFile('./exec.js', function(contentScript){
+
+    // access denied
+    if (data[0] == "error") {
+      var id = gen.next().value;
+      console.log("load page: " + "http://vk.com/audios" + id + '#' + id);
+      mainWindow.loadURL("http://vk.com/audios" + id + '#' + id);
+      mainWindow.webContents.executeJavaScript(contentScript);
+    } else {
+      console.log("Parsed user with ID: " + data[0])
+      console.log("Audios count: " + data.slice(1).length)
+      var doc = {
+        'uid' : data[0],
+        'audios' : data.slice(1)
+      };
+
+      insertDocumentWithUpdate(db, function() {
+        console.log('success update!');
+        var id = gen.next().value;
+        console.log("load page: " + "http://vk.com/audios" + id + '#' + id);
+        mainWindow.loadURL("http://vk.com/audios" + id + '#' + id);
+        mainWindow.webContents.executeJavaScript(contentScript);
+      }, doc);
+    }
+    });
+
+  });
+
+  event.returnValue = 'ok'
+})
+
+var gen = null;
+
+ipcMain.on('parse_run', (event, data) => {
+  MongoClient.connect(conUrl, function(err, db) {
+    assert.equal(null, err);
+
+    var arrayIds = data.match(/[^\r\n]+/g);
+    gen = nextIdGen(arrayIds);
+
+    readFile('./exec.js', function(contentScript){
+      createParseWindow(contentScript, arrayIds);
+    });
+
+  });
+    
+  event.returnValue = 'ok'
+});
+
+ipcMain.on('search_run', (event, data) => {
+  MongoClient.connect(conUrl, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+  });
+
+  event.returnValue = 'ok'
+});
+
+
 
 app.on('ready', function(){
-  createWindow()
 });
 app.on('ready', createCommandWindow);
 
@@ -150,7 +174,6 @@ app.on('window-all-closed', function () {
 })
 app.on('activate', function () {
   if (mainWindow === null) {
-    createWindow()
     createCommandWindow()
   }
 })
